d5b05f5a9cc3deff6b53827daa9529d4a2bfd245
Unquery <ivmsss4@gmail.com>
2025-09-08 22:22:26 +0200

    Added interfaces for repos

---
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/repository/GsrSampleRepository.kt
@@ -0,0 +1,12 @@
+package pl.edu.pjwstk.engineeringthesis.data.repository
+
+import pl.edu.pjwstk.engineeringthesis.model.TempSample
+
+interface GsrSampleRepository {
+    suspend fun addGsrSample(tempSample : TempSample)
+    suspend fun addGsrSample(tempSamples : List<TempSample>)
+    suspend fun removeGsrSample(tempSample : TempSample);
+    suspend fun removeGsrSamplesByEpoch(epoch : Long);
+    suspend fun clear();
+    suspend fun initDb();
+}
---
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/repository/TempSampleRepository.kt
@@ -0,0 +1,12 @@
+package pl.edu.pjwstk.engineeringthesis.data.repository
+
+import pl.edu.pjwstk.engineeringthesis.model.TempSample
+
+interface TempSampleRepository {
+    suspend fun addTempSample(tempSample : TempSample)
+    suspend fun addTempSamples(tempSamples : List<TempSample>)
+    suspend fun removeTempSample(tempSample : TempSample);
+    suspend fun removeTempSamplesByEpoch(epoch : Long);
+    suspend fun clear();
+    suspend fun initDb();
+}
