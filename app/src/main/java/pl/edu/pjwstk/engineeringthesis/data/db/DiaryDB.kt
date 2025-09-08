5f7acccd66e92597f3b4cdea13915162e57b6f45
Unquery <ivmsss4@gmail.com>
2025-09-08 22:33:01 +0200

    Implement DB

---
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
@@ -0,0 +1,19 @@
+package pl.edu.pjwstk.engineeringthesis.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import pl.edu.pjwstk.engineeringthesis.data.db.dao.GsrSampleDao
+import pl.edu.pjwstk.engineeringthesis.data.db.dao.TempSampleDao
+import pl.edu.pjwstk.engineeringthesis.data.db.entity.GsrSampleEntity
+import pl.edu.pjwstk.engineeringthesis.data.db.entity.TempSampleEntity
+
+@Database(
+    entities = [TempSampleEntity::class, GsrSampleEntity::class],
+    version = 1
+)
+abstract class DiaryDB : RoomDatabase() {
+
+    abstract val tempSamples : TempSampleDao
+
+    abstract val gsrSamples : GsrSampleDao
+}
