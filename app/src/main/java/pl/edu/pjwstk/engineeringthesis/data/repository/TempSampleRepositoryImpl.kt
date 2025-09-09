8de779fdef46081cbed27a998e29350d49290907
Unquery <ivmsss4@gmail.com>
2025-09-09 16:23:39 +0200

    Implemented

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/repository/TempSampleRepositoryImpl.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/repository/TempSampleRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package pl.edu.pjwstk.engineeringthesis.data.repository
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import pl.edu.pjwstk.engineeringthesis.data.db.DiaryDB
+import pl.edu.pjwstk.engineeringthesis.data.db.entity.TempSampleEntity
 import pl.edu.pjwstk.engineeringthesis.model.TempSample
 import javax.inject.Inject
 
@@ -13,27 +16,65 @@ class TempSampleRepositoryImpl @Inject constructor(
     val noteItems : List<TempSample>
         get() = _tempSampleItems
 
-    override suspend fun addTempSample(tempSample: TempSample) {
-        TODO("Not yet implemented")
+    override suspend fun getTempSample(id: Int): TempSample? = withContext(Dispatchers.IO) {
+        return@withContext diaryDB.tempSamples.getTempSample(id)?.toDomain()
     }
 
-    override suspend fun addTempSamples(tempSamples: List<TempSample>) {
-        TODO("Not yet implemented")
+    override suspend fun getTempSample (
+        firstEpoch: Long,
+        lastEpoch: Long
+    ): List<TempSample> = withContext(Dispatchers.IO){
+        return@withContext diaryDB.tempSamples.getTempSamples(firstEpoch, lastEpoch).map {
+            it.toDomain()
+        }
     }
 
-    override suspend fun removeTempSample(tempSample: TempSample) {
-        TODO("Not yet implemented")
+    override suspend fun getAllTempSamples(): List<TempSample> = withContext(Dispatchers.IO) {
+        return@withContext diaryDB.tempSamples.getAllTempSample().map{
+            it.toDomain()
+        }
     }
 
-    override suspend fun removeTempSamplesByEpoch(epoch: Long) {
-        TODO("Not yet implemented")
+    override suspend fun addTempSample(tempSample: TempSample) = withContext(Dispatchers.IO) {
+        val tempSampleItem = diaryDB.tempSamples.getTempSample(tempSample.id)
+        if (tempSampleItem == null){
+            diaryDB.tempSamples.addTempSample(
+                TempSampleEntity(tempSample.id, tempSample.epoch, tempSample.temperature)
+            )
+        }else{
+            diaryDB.tempSamples.updateTempSample(
+                TempSampleEntity(tempSampleItem.id, tempSample.epoch, tempSample.temperature)
+            )
+        }
     }
 
-    override suspend fun clear() {
-        TODO("Not yet implemented")
+    override suspend fun addTempSamples(tempSamples: List<TempSample>) = withContext(Dispatchers.IO) {
+        tempSamples.forEach {
+            val tempSampleItem = diaryDB.tempSamples.getTempSample(it.id)
+            if (tempSampleItem == null){
+                diaryDB.tempSamples.addTempSample(
+                    TempSampleEntity(it.id, it.epoch, it.temperature)
+                )
+            }else{
+                diaryDB.tempSamples.updateTempSample(
+                    TempSampleEntity(tempSampleItem.id, it.epoch, it.temperature)
+                )
+            }
+        }
     }
 
-    override suspend fun initDb() {
-        TODO("Not yet implemented")
+    override suspend fun removeTempSample(tempSample: TempSample) = withContext(Dispatchers.IO) {
+        val tempSampleItem = diaryDB.tempSamples.getTempSample(tempSample.id)
+        if(tempSampleItem != null){
+            diaryDB.tempSamples.removeTempSample(tempSampleItem.id)
+        }
+    }
+
+    override suspend fun removeTempSamplesByEpoch(epoch: Long) = withContext(Dispatchers.IO) {
+        diaryDB.tempSamples.removeTempSamplesByEpoch(epoch)
+    }
+
+    override suspend fun clear() = withContext(Dispatchers.IO){
+        diaryDB.tempSamples.removeAllTempSamples()
     }
 }
