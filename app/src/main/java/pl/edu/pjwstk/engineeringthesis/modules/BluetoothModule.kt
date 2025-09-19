3f482518ea5b44426974e0f8868dacc28efc2a90
Unquery <ivmsss4@gmail.com>
2025-09-19 12:45:13 +0200

    Fixed wrong import

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/modules/BluetoothModule.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/modules/BluetoothModule.kt
@@ -8,17 +8,20 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object BluetoothModule {
 
     @Provides
+    @Singleton
     fun provideBluetoothManager(
         @ApplicationContext context: Context
     ): BluetoothManager = context.getSystemService(BluetoothManager::class.java)
 
     @Provides
+    @Singleton
     fun provideBluetoothAdapter(
         manager: BluetoothManager
     ): BluetoothAdapter = manager.adapter
