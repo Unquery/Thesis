970961c88be9d607d063e50c760a0ba734dadec4
Unquery <ivmsss4@gmail.com>
2025-11-06 20:59:13 +0100

    Added loading screen

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/MainActivity.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/MainActivity.kt
@@ -4,26 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import pl.edu.pjwstk.engineeringthesis.ui.theme.EngineeringThesisTheme
 import pl.edu.pjwstk.engineeringthesis.util.Menu
 import pl.edu.pjwstk.engineeringthesis.util.connectBandDestination
 import pl.edu.pjwstk.engineeringthesis.util.menuDestination
+import pl.edu.pjwstk.engineeringthesis.view.SplashOverlay
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
+
         setContent {
             EngineeringThesisTheme {
-               Navigation()
+                AppRoot()
             }
         }
     }
 }
+
+@Composable
+fun AppRoot() {
+    var showSplash by rememberSaveable { mutableStateOf(true) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Navigation()
+
+        if (showSplash) {
+            SplashOverlay(onGone = { showSplash = false })
+        }
+    }
+}
+
 
 @Composable
 fun Navigation(){
