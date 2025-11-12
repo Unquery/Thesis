6055b8a1a470bb101766600f23ae463200ac1396
Unquery <ivmsss4@gmail.com>
2025-11-12 17:32:43 +0100

    Added user profile dao

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import pl.edu.pjwstk.engineeringthesis.data.db.dao.GsrSampleDao
 import pl.edu.pjwstk.engineeringthesis.data.db.dao.TempSampleDao
+import pl.edu.pjwstk.engineeringthesis.data.db.dao.UserProfileDao
 import pl.edu.pjwstk.engineeringthesis.data.db.entity.GsrSampleEntity
 import pl.edu.pjwstk.engineeringthesis.data.db.entity.TempSampleEntity
 
@@ -16,4 +17,7 @@ abstract class DiaryDB : RoomDatabase() {
     abstract val tempSamples : TempSampleDao
 
     abstract val gsrSamples : GsrSampleDao
+
+    abstract val userProfileDao: UserProfileDao
+
 }
