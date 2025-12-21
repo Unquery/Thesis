0b885d66ce12005dd6ca7520aa8b641e01c88f0f
Unquery <ivmsss4@gmail.com>
2025-12-21 22:50:07 +0100

    New version

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
@@ -15,7 +15,7 @@ import pl.edu.pjwstk.engineeringthesis.data.db.entity.UserProfileEntity
 
 @Database(
     entities = [TempSampleEntity::class, GsrSampleEntity::class, UserProfileEntity::class, HearthRateSampleEntity::class, SpO2SampleEntity::class],
-    version = 5
+    version = 6
 )
 abstract class DiaryDB : RoomDatabase() {
 
