48c06d09507e75998cbefd1452fb350c21cfeeb3
Unquery <ivmsss4@gmail.com>
2026-01-13 01:53:47 +0100

    Destinations for device and profile

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/util/ContentKeys.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/util/ContentKeys.kt
@@ -9,5 +9,16 @@ sealed interface Route
 
 @Serializable data object ConnectBand : Route
 
+@Serializable
+enum class ChartMetric {
+    Temperature,
+    HeartRate,
+    SpO2,
+    Gsr
+}
+
+@Serializable data class Charts(val metric: ChartMetric) : Route
+
+@Serializable data object Profile : Route
 
 //Todo keys for other screens
--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/util/Destinations.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/util/Destinations.kt
@@ -3,19 +3,46 @@ package pl.edu.pjwstk.engineeringthesis.util
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import pl.edu.pjwstk.engineeringthesis.view.ChartsScreen
 import pl.edu.pjwstk.engineeringthesis.view.ConnectBandScreen
 import pl.edu.pjwstk.engineeringthesis.view.MenuScreen
+import pl.edu.pjwstk.engineeringthesis.view.ProfileScreen
 
 
 fun NavGraphBuilder.menuDestination(navController: NavController){
     composable<Menu>{
-        MenuScreen(onConnectBandClick = {navController.navigate(ConnectBand)})
+        MenuScreen(
+            onConnectBandClick = { navController.navigate(ConnectBand) },
+            onProfileClick = { navController.navigate(Profile) },
+            onMetricClick = { metric -> navController.navigate(Charts(metric)) }
+        )
     }
 }
 
-fun NavGraphBuilder.connectBandDestination(){
+fun NavGraphBuilder.connectBandDestination(navController: NavController){
     composable<ConnectBand>{
-        ConnectBandScreen()
+        ConnectBandScreen(
+            onHealthClick = { navController.navigate(Menu) },
+            onDeviceClick = {},
+            onProfileClick = { navController.navigate(Profile) }
+        )
     }
 }
 
+fun NavGraphBuilder.chartsDestination(navController: NavController){
+    composable<Charts>{
+        ChartsScreen(
+            onBack = { navController.navigateUp() }
+        )
+    }
+}
+
+fun NavGraphBuilder.profileDestination(navController: NavController){
+    composable<Profile>{
+        ProfileScreen(
+            onHealthClick = { navController.navigate(Menu) },
+            onDeviceClick = { navController.navigate(ConnectBand) },
+            onProfileClick = {}
+        )
+    }
+}
