5d60dd01e67dc750347d192f033f1d5125e59ee0
Unquery <ivmsss4@gmail.com>
2026-04-05 20:08:23 +0200

    Expose SpO2 min-max chart range streams

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/repository/SpO2SampleRepository.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/repository/SpO2SampleRepository.kt
@@ -2,7 +2,9 @@ package pl.edu.pjwstk.engineeringthesis.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import pl.edu.pjwstk.engineeringthesis.model.DailyAvg
+import pl.edu.pjwstk.engineeringthesis.model.DailyMinMax
 import pl.edu.pjwstk.engineeringthesis.model.HourlyAvg
+import pl.edu.pjwstk.engineeringthesis.model.HourlyMinMax
 import pl.edu.pjwstk.engineeringthesis.model.MetricSummary
 import pl.edu.pjwstk.engineeringthesis.model.SpO2Sample
 
@@ -25,6 +27,8 @@ interface SpO2SampleRepository {
 
     fun observeHourlyAvg(userId: Int, startEpoch: Long, endEpoch: Long): Flow<List<HourlyAvg>>
     fun observeDailyAvg(userId: Int, startEpoch: Long, endEpoch: Long): Flow<List<DailyAvg>>
+    fun observeHourlyMinMax(userId: Int, startEpoch: Long, endEpoch: Long): Flow<List<HourlyMinMax>>
+    fun observeDailyMinMax(userId: Int, startEpoch: Long, endEpoch: Long): Flow<List<DailyMinMax>>
     fun observeLatest(userId: Int, startEpoch: Long, endEpoch: Long): Flow<SpO2Sample?>
     fun observeSummary(userId: Int, startEpoch: Long, endEpoch: Long): Flow<MetricSummary>
 
