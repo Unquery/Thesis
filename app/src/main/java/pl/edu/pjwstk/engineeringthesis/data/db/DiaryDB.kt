cc8cd13e76bf2a1dd99ecc4fc2b75596f1c3ed74
Unquery <ivmsss4@gmail.com>
2026-04-15 23:08:45 +0200

    Bump Room database version for GSR float migration

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
@@ -15,7 +15,7 @@ import pl.edu.pjwstk.engineeringthesis.data.db.entity.UserProfileEntity
 
 @Database(
     entities = [TempSampleEntity::class, GsrSampleEntity::class, UserProfileEntity::class, HearthRateSampleEntity::class, SpO2SampleEntity::class],
-    version = 13
+    version = 14
 )
 abstract class DiaryDB : RoomDatabase() {
 
