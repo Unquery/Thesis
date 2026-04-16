af838e1ca1d3a40cced9e42b05e9867b745198f6
Unquery <ivmsss4@gmail.com>
2026-04-16 23:06:53 +0200

    Add shared menu thresholds for conductance intensity

---
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/util/GsrThresholds.kt
@@ -0,0 +1,6 @@
+package pl.edu.pjwstk.engineeringthesis.util
+
+const val GSR_VERY_LOW_THRESHOLD = 1f
+const val GSR_NEUTRAL_MIN = 5f
+const val GSR_NEUTRAL_MAX = 15f
+const val GSR_MENU_MAX_VALUE = 30f
