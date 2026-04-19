e07fd1381604cfbd144a9d44162e0f534bde2218
Unquery <ivmsss4@gmail.com>
2026-04-19 22:15:24 +0200

    Remove the hardcoded band name filter from BLE scanning

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/bluetooth/BleConnectionManager.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/bluetooth/BleConnectionManager.kt
@@ -81,7 +81,6 @@ class BleConnectionManager @Inject constructor(
         UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"),
         UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
     ).apply {
-        setDeviceNameFilter("ESP32-TEMP")
         setPushTimeOnConnect(true)
         setListener(object : BleUartClient.Listener {
             override fun onStatus(s: String) {
