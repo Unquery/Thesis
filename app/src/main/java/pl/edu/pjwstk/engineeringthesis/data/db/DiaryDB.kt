0e1c156aa3c0c3a0fe67fb9a09054de8c1101b3f
Unquery <ivmsss4@gmail.com>
2026-04-20 21:37:53 +0200

    Bump database version for SpO2 calibration fields

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/DiaryDB.kt
@@ -15,7 +15,7 @@ import pl.edu.pjwstk.engineeringthesis.data.db.entity.UserProfileEntity
 
 @Database(
     entities = [TempSampleEntity::class, GsrSampleEntity::class, UserProfileEntity::class, HearthRateSampleEntity::class, SpO2SampleEntity::class],
-    version = 18
+    version = 19
 )
 abstract class DiaryDB : RoomDatabase() {
 
