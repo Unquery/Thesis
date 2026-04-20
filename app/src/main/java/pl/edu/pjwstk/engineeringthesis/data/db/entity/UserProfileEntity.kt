4c62aa580a7caa2920f4d953abbe8df3c59a7e81
Unquery <ivmsss4@gmail.com>
2026-04-20 21:37:53 +0200

    Store SpO2 normal range in user profile entity

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/UserProfileEntity.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/UserProfileEntity.kt
@@ -6,6 +6,8 @@ import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_HEART_RATE_HIGH
 import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_HEART_RATE_LOW
 import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_SKIN_CONDUCTANCE_HIGH
 import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_SKIN_CONDUCTANCE_LOW
+import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_SPO2_HIGH
+import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_SPO2_LOW
 import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_TEMPERATURE_HIGH
 import pl.edu.pjwstk.engineeringthesis.util.PROFILE_DEFAULT_TEMPERATURE_LOW
 
@@ -22,6 +24,8 @@ data class UserProfileEntity(
     val temperatureNormalHigh: Float = PROFILE_DEFAULT_TEMPERATURE_HIGH,
     val heartRateNormalLow: Float = PROFILE_DEFAULT_HEART_RATE_LOW,
     val heartRateNormalHigh: Float = PROFILE_DEFAULT_HEART_RATE_HIGH,
+    val spO2NormalLow: Float = PROFILE_DEFAULT_SPO2_LOW,
+    val spO2NormalHigh: Float = PROFILE_DEFAULT_SPO2_HIGH,
     val skinConductanceNormalLow: Float = PROFILE_DEFAULT_SKIN_CONDUCTANCE_LOW,
     val skinConductanceNormalHigh: Float = PROFILE_DEFAULT_SKIN_CONDUCTANCE_HIGH
 )
