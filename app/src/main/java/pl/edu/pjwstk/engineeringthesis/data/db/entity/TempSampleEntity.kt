28c54a147a63538d0dbdd5582d710465e3215503
Unquery <ivmsss4@gmail.com>
2026-04-27 23:06:33 +0200

    Index temperature samples by user and epoch

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/TempSampleEntity.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/TempSampleEntity.kt
@@ -1,10 +1,14 @@
 package pl.edu.pjwstk.engineeringthesis.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import pl.edu.pjwstk.engineeringthesis.model.TempSample
 
-@Entity(tableName = "temp_sample")
+@Entity(
+    tableName = "temp_sample",
+    indices = [Index(value = ["userId", "epoch"])]
+)
 data class TempSampleEntity(
     @PrimaryKey(autoGenerate = true)
     val id : Int = 0,
