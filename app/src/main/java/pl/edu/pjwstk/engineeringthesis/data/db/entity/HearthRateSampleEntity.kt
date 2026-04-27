e1be5db65cd900e8fedf150b88e052ec85482f6c
Unquery <ivmsss4@gmail.com>
2026-04-27 23:06:33 +0200

    Index heart rate samples by user and epoch

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/HearthRateSampleEntity.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/HearthRateSampleEntity.kt
@@ -1,10 +1,14 @@
 package pl.edu.pjwstk.engineeringthesis.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import pl.edu.pjwstk.engineeringthesis.model.HearthRateSample
 
-@Entity(tableName = "hearth_rate_sample")
+@Entity(
+    tableName = "hearth_rate_sample",
+    indices = [Index(value = ["userId", "epoch"])]
+)
 data class HearthRateSampleEntity(
     @PrimaryKey(autoGenerate = true)
     val id : Int = 0,
