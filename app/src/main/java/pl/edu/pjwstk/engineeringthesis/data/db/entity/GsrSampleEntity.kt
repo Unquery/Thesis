1552961206f4655d3eac50bb6ec12d6ce9c04e1b
Unquery <ivmsss4@gmail.com>
2026-04-27 23:06:33 +0200

    Index GSR samples by user and epoch

--- app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/GsrSampleEntity.kt
+++ app/src/main/java/pl/edu/pjwstk/engineeringthesis/data/db/entity/GsrSampleEntity.kt
@@ -1,10 +1,14 @@
 package pl.edu.pjwstk.engineeringthesis.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import pl.edu.pjwstk.engineeringthesis.model.GsrSample
 
-@Entity(tableName = "gsr_sample")
+@Entity(
+    tableName = "gsr_sample",
+    indices = [Index(value = ["userId", "epoch"])]
+)
 data class GsrSampleEntity(
     @PrimaryKey(autoGenerate = true)
     val id : Int = 0,
